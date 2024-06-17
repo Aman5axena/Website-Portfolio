495084a3381a18eb9789f4853c09b13c0d5e5b0c
Aman5axena <164467902+Aman5axena@users.noreply.github.com>
2024-06-17 22:29:09 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrolly;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+// Add this to your scripts.js
+function transitionToPage(href) {
+    document.body.classList.add('fade-out');
+    setTimeout(function() { window.location.href = href; }, 500);
+  }
+
+window.addEventListener('DOMContentLoaded', (event) => {
+   document.body.classList.remove('fade-out');
+});
